a83e052bca7a52f461fea289cddf90b2d25e1ac8
chrrispin <chrrispin@gmail.com>
2026-01-01 15:12:35 +0200

    header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,13 +3,17 @@ const Header: React.FC = () => {
         <>
         {/* HEADER */}
       <header className="border-b border-gray-100">
-        <div id="Hdiv" className="px-4 py-5 flex items-center justify-center relative">
+        <a
+          href="https://newtimeafrica.com/"
+          className="px-4 py-5 flex items-center justify-center relative block"
+          aria-label="New Time Africa home"
+        >
           <h1 className="font-semibold text-xl ">  
             <span className="text-red-900"> New</span>
             <span className="text-blue-900"> Time</span>
             <span className="text-green-600"> Africa</span>
           </h1>
-        </div>
+        </a>
       </header>
         </>
     )
